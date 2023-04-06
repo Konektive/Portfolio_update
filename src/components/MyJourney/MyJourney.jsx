b5fd88d970e8429dee9bfcb12b3a25882c4a4528
Krystian <krystiansporysz@gmail.com>
2023-04-06 10:36:38 +0200

    Animations
    Added some animations on scroll to myJourney elements

--- src/components/MyJourney/MyJourney.jsx
+++ src/components/MyJourney/MyJourney.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import styles from './MyJourney.module.css'
+import {useInView} from 'react-intersection-observer'
 
 const MyJourney = () => {
+const {ref: stackRef, inView:stackIsVisible} = useInView()
+
+
   return (
     <div id="myjourney" className={styles.journey}>
       <article>
         <title>My Journey</title>
         <h2>How IT all started!</h2>
       </article>
-      <div className={styles.steps}>
-        <div className="step1">
+      <div ref={stackRef} className={styles.steps}>
+        <div className={`${styles.stepOne} ${stackIsVisible ? styles.show : ''}`}>
           <h3>01</h3>
           <h4>
             <span>School, </span>
@@ -19,17 +23,17 @@ const MyJourney = () => {
             Python.
           </p>
         </div>
-        <div className="step2">
+        <div className={`${styles.stepTwo} ${stackIsVisible ? styles.show : ''}`}>
           <h3>02</h3>
-          <h4>Personal Trainer, IT break</h4>
+          <h4><span>Personal Trainer</span>, IT break</h4>
           <p>
             Personal Trainer certification, started working on local gym,
             took a break from IT.
           </p>
         </div>
-        <div className="step3">
+        <div className={`${styles.stepThree} ${stackIsVisible ? styles.show : ''}`}>
           <h3>03</h3>
-          <h4>Back to the roots!</h4>
+          <h4><span>Back</span> to the roots!</h4>
           <p>
             Started grinding on programming, learned React, TypeScript, Advanced
             JavaScript
