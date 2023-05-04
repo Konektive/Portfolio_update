4d564126af52bd146abd9a08929dea1312181803
Hanna Pęciak <103579210+haszpe@users.noreply.github.com>
2023-05-04 14:37:52 +0200

    Added form and made some adjustments with Insta scroll item

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,36 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import styles from './Contact.module.css'
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <div className={styles.formWrap}>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+    <aside className={styles.socials}>Insta Facebook Linked</aside>
+
+    </div>
+  );
+};
+
+export default Contact;
