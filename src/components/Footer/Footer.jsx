bfd4aada43b0010eadfe9384f183431a29467208
Krystian <krystiansporysz@gmail.com>
2023-05-12 23:07:42 +0200

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import styles from './Footer.module.css'
+import React from "react";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <div className={styles.footer}><h3><a href="mailto:krystiansporysz@gmail.com">Email Me directly!</a></h3></div>
-  )
-}
+    <div className={styles.footer}>
+      <section>
+      <h2>
+        <a href="mailto:krystiansporysz@gmail.com">krystiansporysz@gmail.com</a>
+      </h2>
+      <h2><a href="tel:+48695444638">+48 695444638</a></h2>
+      </section>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
