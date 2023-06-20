36da1cc81eda6ca2b0867d0cb3bb8af4f5d96d0d
Krystian <krystiansporysz@gmail.com>
2023-06-20 08:29:48 +0200

    Trying different apearance on Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import { BsTelephone } from "react-icons/bs";
+import { BsTelephone,BsHouseDoor } from "react-icons/bs";
 import { SiGmail } from "react-icons/si";
 
 const Footer = () => {
@@ -10,7 +10,7 @@ const Footer = () => {
         <h2>Usefull links</h2>
       </div>
       <div className={styles.contact}>
-        <h3>Contact</h3>
+        <p>Direct Contact</p>
         <p>
           <SiGmail />{" "}
           <a href="https://mail.google.com/mail/?view=cm&fs=1&to=krystiansporysz@gmail.com">
@@ -20,6 +20,10 @@ const Footer = () => {
         <p>
           <BsTelephone />
           695444638
+        </p>
+        <p>
+          <BsHouseDoor/>
+          Wrocław, ul. Kołłątaja 27/26
         </p>
       </div>
     </div>
