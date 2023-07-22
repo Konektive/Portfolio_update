2e3d3d30fe32a0419940f9c69589cfa70762aa82
Krystian <krystiansporysz@gmail.com>
2023-07-22 20:50:59 +0200

    Addad button activated projects

--- src/components/Portfolio/Projects.jsx
+++ src/components/Portfolio/Projects.jsx
@@ -1,3 +1,38 @@
+const DUMMY_PROJECTS = [
+  {
+    id: 1,
+    title: "weather app",
+    description:
+      "Real-time weather updates based on your location, keeping you prepared for the current weather conditions.",
+    technologies: ["html"],
+    purpose: "Accurate Forecasts",
+    code: "https://github.com/Konektive/Weather-App",
+    link: "https://media.discordapp.net/attachments/1100137809932722198/1100137834234519692/image.png?width=873&height=440",
+    site: "https://app.netlify.com/teams/konektive/overview",
+  },
+  {
+    id: 2,
+    title: "Restaurant app",
+    description:
+      "Seamless online food ordering from a featured restaurant, bringing the convenience of delicious meals to your fingertips.",
+    technologies: ["js"],
+    purpose: "Easy Dining Solutions",
+    code: "https://github.com/",
+    link: "https://cdn.discordapp.com/attachments/1100137809932722198/1100154197866983626/platter-2009590_1920.jpg",
+    site: "https://app.netlify.com/teams/konektive/overview",
+  },
+  {
+    id: 3,
+    title: "Blog app",
+    description:
+      "Engaging and informative text content covering diverse topics to inspire and entertain readers with valuable insights.",
+    technologies: ["react"],
+    purpose: "Creative Storytelling",
+    code: "https://github.com/",
+    link: "https://cdn.discordapp.com/attachments/1100137809932722198/1100153870849675384/blog-793047_1920.jpg",
+    site: "https://app.netlify.com/teams/konektive/overview",
+  },
+];
 import React, { useState } from "react";
 import Single from "./Single";
 import styles from "./Projects.module.css";
@@ -7,10 +42,9 @@ const Projects = () => {
 
   const pickStack = (value) => {
     if (stack === value) {
-      setStack('')
-    }
-    else (setStack(value))
-    console.log(stack)
+      setStack("");
+    } else setStack(value);
+    console.log(stack);
   };
 
   return (
@@ -25,7 +59,7 @@ const Projects = () => {
       </button>
       <button
         onClick={() => {
-          pickStack('js');
+          pickStack("js");
         }}
       >
         JavaScript
@@ -45,7 +79,25 @@ const Projects = () => {
         TypeScript
       </button>
       <div className={styles.projects} id="projects">
-        <Single selected={stack} />
+        {DUMMY_PROJECTS.filter((project) => {
+          if (stack === "") {
+            return project;
+          } else if (project.technologies.includes(stack)) {
+            return project;
+          }
+        }).map((project) => {
+          return (
+            <Single
+              key={project.id}
+              title={project.title}
+              code={project.code}
+              link={project.link}
+              site={project.site}
+              purpose={project.purpose}
+              description={project.description}
+            />
+          );
+        })}
       </div>
     </>
   );
--- src/components/Portfolio/Single.jsx
+++ src/components/Portfolio/Single.jsx
@@ -1,54 +1,26 @@
 import React from "react";
 import styles from "./Single.module.css";
 const Single = (props) => {
-  const DUMMY_PROJECTS = [
-    {
-      title: "weather app",
-      description:"Real-time weather updates based on your location, keeping you prepared for the current weather conditions.",
-      technologies: ["html"],
-      purpose:'Accurate Forecasts',
-      code:"https://github.com/Konektive/Weather-App",
-      link:'https://media.discordapp.net/attachments/1100137809932722198/1100137834234519692/image.png?width=873&height=440',
-      site:"https://app.netlify.com/teams/konektive/overview"
-    },
-    {
-      title: "Restaurant app",
-      description: "Seamless online food ordering from a featured restaurant, bringing the convenience of delicious meals to your fingertips.",
-      technologies: ["js"],
-      purpose:'Easy Dining Solutions',
-      code:"https://github.com/",
-      link:'https://cdn.discordapp.com/attachments/1100137809932722198/1100154197866983626/platter-2009590_1920.jpg',
-      site:"https://app.netlify.com/teams/konektive/overview"
-    },
-    {
-      title: "Blog app",
-      description:"Engaging and informative text content covering diverse topics to inspire and entertain readers with valuable insights.",
-      technologies: ["react"],
-      purpose:"Creative Storytelling",
-      code:"https://github.com/",
-      link:'https://cdn.discordapp.com/attachments/1100137809932722198/1100153870849675384/blog-793047_1920.jpg',
-      site:"https://app.netlify.com/teams/konektive/overview"
-    },
-  ];
+  const { title, code, id, link, site, purpose, description } = props;
 
   return (
-    <>
-      {DUMMY_PROJECTS.filter(stack => stack.technologies.includes(props.selected)).map((project) => {
-        return (
-          <div key={project.title} className={styles.project}>
-            <p>{project.title}</p>
-            <a className={styles.code} href={project.code}>Code</a>
-            <div className={styles.glass}>
-              <a style={{backgroundImage: `url(${project.link})`}} className={styles.image} href={project.site}></a>
-            </div>
-            <div className={styles.description}>
-              <h3>{project.purpose}</h3>
-              <p>{project.description}</p>
-            </div>
-          </div>
-        );
-      })}
-    </>
+    <div key={title} className={styles.project}>
+      <p>{title}</p>
+      <a className={styles.code} href={code}>
+        Code
+      </a>
+      <div className={styles.glass}>
+        <a
+          style={{ backgroundImage: `url(${link})` }}
+          className={styles.image}
+          href={site}
+        ></a>
+      </div>
+      <div className={styles.description}>
+        <h3>{purpose}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
   );
 };
 
