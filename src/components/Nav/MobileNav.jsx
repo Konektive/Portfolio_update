9365b89bdfa0eab1b4169313c69f195d762813e0
Krystian <krystiansporysz@gmail.com>
2023-08-08 20:23:45 +0200

    Mobile nav improvement

--- src/components/Nav/MobileNav.jsx
+++ src/components/Nav/MobileNav.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import styles from './MobileNav.module.css'
+import React from "react";
+import { useState } from "react";
+import styles from "./MobileNav.module.css";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillCalendarDateFill } from "react-icons/bs";
+import { BsPersonWorkspace } from "react-icons/bs";
+import { SiAboutdotme } from "react-icons/si";
+import { HiMail } from "react-icons/hi";
 
 const MobileNav = () => {
-  return (
-    <div className={styles.mobile}>Mobile</div>
-  )
-}
+  const [menu, setMenu] = useState('home');
 
-export default MobileNav
+  return (
+    <div className={styles.mobile}>
+      <a href="#myjourney" className={menu === "story" ? styles.active : undefined}  onClick={()=>{
+        setMenu('story')
+      }}>
+        <BsFillCalendarDateFill />
+      </a>
+      <a href="#projects" className={menu === "work" ? styles.active : undefined}  onClick={()=>{
+        setMenu('work')
+      }}>
+        <BsPersonWorkspace />
+      </a>
+      <a href="#home" className={menu === "home" ? styles.active : undefined}  onClick={()=>{
+        setMenu('home')
+      }}>
+        <AiFillHome />
+      </a>
+      <a href="#about" className={menu === "about" ? styles.active : undefined}  onClick={()=>{
+        setMenu('about')
+      }}>
+        <SiAboutdotme />
+      </a>
+      <a href="#contact" className={menu === "contact" ? styles.active : undefined}  onClick={()=>{
+        setMenu('contact')
+      }}>
+        <HiMail />
+      </a>
+    </div>
+  );
+};
+
+export default MobileNav;
